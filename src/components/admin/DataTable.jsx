6edba200a859c8834767277aee6b9d60f448eb97
chuheeseung <youverygood@naver.com>
2023-03-15 16:37:06 +0900

    [feat, style] Admin header 구현

---
+++ src/components/admin/DataTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DataTable(props) {
+	return <div>DataTable</div>;
+}
+
+export default DataTable;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import DataTable from '../components/admin/DataTable';
+import { TiThMenu } from 'react-icons/ti';
+import ModalContainer from '../components/admin/ModalContainer';
 
 function Admin(props) {
+	const [showModal, setShowModal] = useState(false);
+
+	const handleClick = () => {
+		setShowModal(!showModal);
+	};
+
 	return (
 		<div>
-			<div>관리자 페이지</div>
+			<Header>
+				<MenuButton onClick={handleClick}>
+					<TiThMenu />
+				</MenuButton>
+				{showModal ? <ModalContainer showModal={showModal} /> : null}
+				<AdminTitle>관리자 페이지</AdminTitle>
+			</Header>
+			<DataTable />
 		</div>
 	);
 }
 
 export default Admin;
+
+const Header = styled.header`
+	width: 100%;
+	padding: 1rem 0;
+	display: flex;
+	align-items: center;
+	background-color: #14b560;
+`;
+
+const MenuButton = styled.button`
+	border: none;
+	background-color: transparent;
+	align-items: center;
+	font-size: 2.4rem;
+
+	:hover {
+		transition: all ease 0.1s;
+		transform: scale(1.02);
+	}
+`;
+
+const AdminTitle = styled.div`
+	margin: 0 auto;
+	align-items: center;
+	font-size: 2rem;
+`;
