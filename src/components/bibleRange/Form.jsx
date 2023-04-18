82ddc16b8bc54f500aa4f6b19230385bc1964fb1
chuheeseung <youverygood@naver.com>
2023-04-18 23:24:09 +0900

    [feat] 통독범위 입력 페이지 월별로 분리 [style] InputForm css 적용

--- src/components/bibleRange/Form.jsx
+++ src/components/bibleRange/Form.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Form({ month, date }) {
 	return (
-		<div>
-			<span>
+		<InputWrap>
+			<DateSpan>
 				{month}월 {date}일
-			</span>
-			<input type="text" />
-		</div>
+			</DateSpan>
+			<DateInput type="text" />
+		</InputWrap>
 	);
 }
 
 export default Form;
+
+const InputWrap = styled.div`
+	/* display: flex; */
+	margin-bottom: 0.5rem;
+	margin-right: 0.2rem;
+`;
+
+const DateSpan = styled.span`
+	font-size: 1.2rem;
+`;
+
+const DateInput = styled.input``;
--- src/components/bibleRange/InputForms.jsx
+++ src/components/bibleRange/InputForms.jsx
@@ -1,29 +1,49 @@
 import React from 'react';
 import Form from './Form';
+import styled from 'styled-components';
 
 export const dates = [31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function InputForms() {
-	const setInputs = () => {
+	const setInputs = (date, index) => {
 		let inputs = [];
 
-		for (let i = 0; i < dates.length; i++) {
-			for (let j = 0; j < dates[i]; j++) {
-				inputs.push(
-					<Form key={`${i + 1}_${j + 1}`} month={i + 1} date={j + 1} />
-				);
-			}
+		for (let j = 0; j < date; j++) {
+			inputs.push(
+				<Form key={`${index + 1}_${j + 1}`} month={index + 1} date={j + 1} />
+			);
 		}
 
 		return inputs;
 	};
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('클릭되었습니다.');
+	};
+
 	return (
-		<form>
-			{setInputs()}
+		<FormContainer onSubmit={handleSubmit}>
+			<FlexWrap>
+				{dates.map((date, index) => (
+					<MonthWrap>{setInputs(date, index)}</MonthWrap>
+				))}
+			</FlexWrap>
 			<button>저장 버튼</button>
-		</form>
+		</FormContainer>
 	);
 }
 
 export default InputForms;
+
+const FormContainer = styled.form``;
+
+const FlexWrap = styled.div`
+	width: 90%;
+	height: 70vh;
+	margin: 0 auto;
+	overflow: scroll;
+	display: flex;
+`;
+
+const MonthWrap = styled.div``;
