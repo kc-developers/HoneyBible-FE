47e716c5e648e8674d50ade8bdc6939ca632fc98
chuheeseung <youverygood@naver.com>
2023-04-26 17:17:01 +0900

    [feat] 인트로페이지 레이아웃 구현

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-function Intro(props) {
-	return <div>Intro</div>;
+function Intro() {
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate('/login');
+	};
+
+	return (
+		<IntroDiv>
+			<HoneyImage src="./assets/honey.svg" alt="honeyLogo" />
+			<IntroText>
+				<div>맑청하!</div>
+				<div>어서오세요,</div>
+				<div>
+					<YellowText>꿀성경</YellowText>
+					<span>입니다.</span>
+				</div>
+				<div>맑은샘광천교회 청년부</div>
+			</IntroText>
+			<IntroButton onClick={handleClick}>탭해서 시작하기</IntroButton>
+		</IntroDiv>
+	);
 }
 
 export default Intro;
+
+const IntroDiv = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-color: var(--color-background-black);
+`;
+
+const HoneyImage = styled.img`
+	top: 10%;
+`;
+
+const IntroText = styled.div`
+	color: #ffffff;
+`;
+
+const YellowText = styled.span`
+	color: var(--color-yellow);
+`;
+
+const IntroButton = styled.button`
+	width: 30%;
+	margin: 0 auto;
+	text-align: center;
+	border: none;
+	background-color: var(--color-background-black);
+	color: #ffffff;
+`;
