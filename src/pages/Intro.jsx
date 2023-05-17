323ec3dc4f27f3b43cb30244db63e74f08060ce4
chuheeseung <youverygood@naver.com>
2023-05-17 22:15:51 +0900

    no message

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -13,13 +13,17 @@ function Intro() {
 		<IntroDiv>
 			<HoneyImage src="./assets/honey.svg" alt="honeyLogo" />
 			<IntroText>
-				<div>맑청하!</div>
-				<div>어서오세요,</div>
-				<div>
-					<YellowText>꿀성경</YellowText>
-					<span>입니다.</span>
-				</div>
-				<div>맑은샘광천교회 청년부</div>
+				<IntroBig>
+					<div>맑청하!</div>
+					<div>어서오세요,</div>
+					<div>
+						<YellowText>꿀성경</YellowText>
+						<span>입니다.</span>
+					</div>
+				</IntroBig>
+				<IntroSmall>
+					<div>맑은샘광천교회 청년부</div>
+				</IntroSmall>
 			</IntroText>
 			<IntroButton onClick={handleClick}>탭해서 시작하기</IntroButton>
 		</IntroDiv>
@@ -42,15 +46,28 @@ const IntroText = styled.div`
 	color: #ffffff;
 `;
 
+const IntroBig = styled.div`
+	font-size: 4rem;
+	letter-spacing: -0.03em;
+`;
+
+const IntroSmall = styled.div`
+	margin-top: 2rem;
+	font-size: 1.6rem;
+`;
+
 const YellowText = styled.span`
 	color: var(--color-yellow);
+	font-weight: 700;
 `;
 
 const IntroButton = styled.button`
-	width: 30%;
+	/* width: 30%; */
 	margin: 0 auto;
 	text-align: center;
 	border: none;
 	background-color: var(--color-background-black);
 	color: #ffffff;
+	font-weight: 500;
+	font-size: 2rem;
 `;
