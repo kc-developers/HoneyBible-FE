60860303a3968e26376d2a4efc98cebbd9cdd43c
chuheeseung <youverygood@naver.com>
2023-06-08 22:58:31 +0900

    [refactor] css 파일 분리

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import styles from './Intro.module.css';
 
 function Intro() {
 	const navigate = useNavigate();
@@ -10,64 +10,24 @@ function Intro() {
 	};
 
 	return (
-		<IntroDiv>
-			<HoneyImage src="./assets/honey.svg" alt="honeyLogo" />
-			<IntroText>
-				<IntroBig>
+		<div className={styles.container}>
+			<img className={styles.logo} src="./assets/honey.svg" alt="logo" />
+			<div className={styles.textWrap}>
+				<div className={styles.textBig}>
 					<div>맑청하!!!!</div>
-					<div>어서오세요,</div>
+					<div>어서요세요,</div>
 					<div>
-						<YellowText>꿀성경</YellowText>
+						<span className={styles.yellowText}>꿀성경</span>
 						<span>입니다.</span>
 					</div>
-				</IntroBig>
-				<IntroSmall>
-					<div>맑은샘광천교회 청년부</div>
-				</IntroSmall>
-			</IntroText>
-			<IntroButton onClick={handleClick}>탭해서 시작하기</IntroButton>
-		</IntroDiv>
+				</div>
+				<div>맑은샘광천교회 청년부</div>
+			</div>
+			<button className={styles.tabButton} onClick={handleClick}>
+				탭해서 시작하기
+			</button>
+		</div>
 	);
 }
 
 export default Intro;
-
-const IntroDiv = styled.div`
-	width: 100vw;
-	height: 100vh;
-	background-color: var(--color-background-black);
-`;
-
-const HoneyImage = styled.img`
-	top: 10%;
-`;
-
-const IntroText = styled.div`
-	color: #ffffff;
-`;
-
-const IntroBig = styled.div`
-	font-size: 4rem;
-	letter-spacing: -0.03em;
-`;
-
-const IntroSmall = styled.div`
-	margin-top: 2rem;
-	font-size: 1.6rem;
-`;
-
-const YellowText = styled.span`
-	color: var(--color-yellow);
-	font-weight: 700;
-`;
-
-const IntroButton = styled.button`
-	/* width: 30%; */
-	margin: 0 auto;
-	text-align: center;
-	border: none;
-	background-color: var(--color-background-black);
-	color: #ffffff;
-	font-weight: 500;
-	font-size: 2rem;
-`;
