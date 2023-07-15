a2310650db3c0faea7d555f538a0616d5d89cb6a
chuheeseung <youverygood@naver.com>
2023-07-15 16:54:45 +0900

    [style] 헤더 버튼 햄버거메뉴 삭제

--- src/components/admin/header/Header.jsx
+++ src/components/admin/header/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
-import { TiThMenu } from 'react-icons/ti';
 import ModalContainer from './ModalContainer';
 
 function Header({ text }) {
@@ -14,10 +13,7 @@ function Header({ text }) {
 		<div className={styles.container}>
 			<div className={styles.title}>{text}</div>
 			<div>
-				<button className={styles.button} onClick={handleClick}>
-					<TiThMenu />
-				</button>
-				<div>{showModal ? <ModalContainer showModal={showModal} /> : null}</div>
+				<ModalContainer />
 			</div>
 		</div>
 	);
