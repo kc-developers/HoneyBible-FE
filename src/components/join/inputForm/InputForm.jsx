2f81465574598095b4acbc178cf90500dc1c2327
chuheeseung <youverygood@naver.com>
2023-09-29 17:15:23 +0900

    [feat] 통신 전까지 구현

--- src/components/join/inputForm/InputForm.jsx
+++ src/components/join/inputForm/InputForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from './InputForm.module.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { options } from './options';
 
 function InputForm({ type }) {
 	const [age, setAge] = useState(null);
@@ -39,23 +40,46 @@ function InputForm({ type }) {
 		}
 	};
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-
+	const handleSubmit = async (e) => {
 		if (nameValid && ageValid) {
+			e.preventDefault();
+
 			if (type === 'join') {
+				console.log(name, age);
+
+				const result = await axios({
+					headers: {},
+					method: 'POST',
+					url: 'https://localhost:8080/auth/join', // url 수정
+					data: {
+						name: name,
+						age: age,
+					},
+				}).then((res) => {
+					console.log(res);
+				});
+
+				alert('회원가입이 완료되었습니다.');
 				navigate('/login');
-				// axios
-				// 	.get('https://localhost:8080/api/admin')
-				// 	.then((res) => {
-				// 		setData((prev) => [res.data.data]);
-				// 	})
-				// 	.catch((err) => {
-				// 		console.log(err);
-				// 	});
-				// const result = axios.push('https://localhost:8080/auth/join');
 			} else {
 				// type === 'login'
+				console.log(name, age);
+
+				const result = await axios({
+					headers: {},
+					method: 'POST',
+					url: 'https://localhost:8080/auth/login', // url 수정
+					data: {
+						name: name,
+						age: age,
+					},
+				}).then((res) => {
+					console.log(res);
+					// localStorage에 토큰 저장하거나 할 일 추가하기
+
+					alert('로그인에 성공했습니다.');
+					navigate('/datepicker');
+				});
 			}
 		}
 	};
@@ -111,29 +135,3 @@ function InputForm({ type }) {
 }
 
 export default InputForm;
-
-const options = [
-	{ value: '04', label: '04또래' },
-	{ value: '03', label: '03또래' },
-	{ value: '02', label: '02또래' },
-	{ value: '01', label: '01또래' },
-	{ value: '00', label: '00또래' },
-	{ value: '99', label: '99또래' },
-	{ value: '98', label: '98또래' },
-	{ value: '97', label: '97또래' },
-	{ value: '96', label: '96또래' },
-	{ value: '95', label: '95또래' },
-	{ value: '94', label: '94또래' },
-	{ value: '93', label: '93또래' },
-	{ value: '92', label: '92또래' },
-	{ value: '91', label: '91또래' },
-	{ value: '90', label: '90또래' },
-	{ value: '89', label: '89또래' },
-	{ value: '88', label: '88또래' },
-	{ value: '87', label: '87또래' },
-	{ value: '86', label: '86또래' },
-	{ value: '85', label: '85또래' },
-	{ value: '84', label: '84또래' },
-	{ value: '83', label: '83또래' },
-	{ value: '82', label: '82또래' },
-];
