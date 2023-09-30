b65cbee5282b2fb2de95dbb27a165a6304e681f0
tjsdus1354 <tjsdus1354@naver.com>
2023-09-30 15:57:56 +0900

    first calendar css

--- src/pages/DatePicker.jsx
+++ src/pages/DatePicker.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // css import
 
 function DatePicker(props) {
-	return <div>DatePicker</div>;
+	const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
 }
 
 export default DatePicker;
